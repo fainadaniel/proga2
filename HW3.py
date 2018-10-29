6f92893a8bd78d563ba22425cdbcba4b328167e8
fainadaniel <faina.daniel@gmail.com>
2018-10-29 08:58:33 +0300

    Add files via upload

---
+++ HW3.py
@@ -0,0 +1,7 @@
+def table:
+    table = [path, author, header, created sphere, topic, style, audience_age, audience_level, audience_size, source, publication, publ_year medium, country, region, language]
+    return table
+main(table):
+    print(table)
+    return
+main(table)
